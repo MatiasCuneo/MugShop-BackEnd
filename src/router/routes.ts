7a8885a41297e51852632877e3d132cb86871729
Cuneo Bonne Matías Nicolás <m.cuneo@etec.local>
2023-10-20 12:23:37 -0300

    fix

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,6 +1,6 @@
-import express, { Router } from 'express';
+import { Router } from 'express';
 
-const router: Router = express.Router();
+const router: Router = Router();
 
 router.get('/', (_, res) => {
     res.send('Test');
