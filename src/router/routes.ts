027c16fb92fa220c61895d47c862d7f9160fef2a
Cuneo Bonne Matías Nicolás <m.cuneo@etec.local>
2023-10-25 13:08:03 -0300

    wip

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -2,8 +2,57 @@ import { Router } from 'express';
 
 const router: Router = Router();
 
+const products = [
+    {nombre:"Taza", modelo:"Grande", pais:"Tailandia", precio:50},
+    {nombre:"Jarrito", modelo:"Hondo", pais:"Italia", precio:20},
+    {nombre:"Disco", modelo:"Acero", pais:"Argentina", precio:120}
+]; 
+
 router.get('/', (_, res) => {
-    res.send('Test');
+    res.json(products);
 });
+
+router.get('/mayora100', (_, res) => {
+    res.json(products.filter(producto => producto.precio > 100));
+});
+
+
+router.delete('/borrar/:modelo', (req, res) => {
+    const { modelo } = req.params;
+    console.log(modelo);
+    const i = products.findIndex(producto => producto.modelo === modelo);
+    console.log(i);
+
+    if (i >= 0) {
+        products.splice(i, 1);
+        res.send("Producto eliminado exitosamente");
+    } else {
+        res.status(404).send("No se pudo encontrar el producto");
+    }
+});
+
+router.get('/productos/pais/:paisOrigen', (req, res) => {
+    const { paisOrigen } = req.params;
+    const producto = products.find(producto => producto.pais === paisOrigen);
+  
+    if (producto) {
+      res.json(producto);
+    } else {
+      res.status(404).json({ message: 'Producto no encontrado' });
+    }
+  });
+  
+
+// ---------------------------------------
+
+
+
+// const products = [
+//     {nombre:"Taza", modelo:"Grande", pais:"Tailandia", precio:50},
+//     {nombre:"Jarrito", modelo:"Hondo", pais:"Italia", precio:20},
+//     {nombre:"Disco", modelo:"Acero", pais:"Argentina", precio:120}
+// ]; 
+
+// ---------------------------------------
 
 export default router;
